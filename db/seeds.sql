4bd7ea5fb66dced12961d1fa35bebe9ca866c5c5
Mrrwmix <seidoc@gmail.com>
2019-08-01 18:06:37 -0700

    DB created

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ("Super Burger", true);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Ultra Burger", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Mediocre Burger", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Mystery Meat Burger", false);
