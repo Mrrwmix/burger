0791d63d713dd158a1bd703579d6f6159ff4ee20
Mrrwmix <seidoc@gmail.com>
2019-08-02 13:32:51 -0700

    Some styling changes, working POST and GET methods

--- public/assets/js/burgers.js
+++ public/assets/js/burgers.js
@@ -1,6 +1,19 @@
-$("body").on("click", "#submit", function(e) {
-  e.preventDefault();
-  console.log("Wot");
+$(document).ready(function() {
+  $("body").on("click", "#submit", function() {
+    console.log("Wot");
+    $.ajax({
+      url: "/api/burgers",
+      method: "POST",
+      data: {
+        name: $("#name")
+          .val()
+          .trim()
+      }
+    }).done(function(msg) {
+      console.log(msg);
+      document.location.reload();
+    });
+  });
 });
 
 console.log("HIHIHI");
